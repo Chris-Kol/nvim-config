50cbd4771bece4dbe8fc1eeedd0cd71ae502cb4a
Chris-Kol <ckoleri@epignosishq.com>
2025-03-23 15:14:52 +0200

    initial commit

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,15 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    transparent_background = false,
+    show_end_of_buffer = false,
+  },
+  -- Apply the colorscheme after it loads
+  init = function()
+    -- This still runs during startup, but only after the plugin is loaded
+    vim.cmd.colorscheme "catppuccin"
+  end,
+}
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,10 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  init = function()
+    require('lualine').setup({
+      options = {
+        theme = 'dracula'
+      }
+    })
+  end,
+}
